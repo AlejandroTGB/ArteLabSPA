ae538653a22b7a68023166d4cab45efd6280663f
Alejandro Torres <alej.torresg@duocuc.cl>
2025-12-12 16:02:53 -0300

    Agregadas dependencias: retrofit y gson

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -75,6 +75,8 @@ dependencies {
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(platform(libs.androidx.compose.bom))
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
 
     tasks.withType<Test> {
         testLogging {
